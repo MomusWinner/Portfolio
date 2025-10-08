6988acfc0842f57a30957ebc1dd5ca937bcc1572
MomusWinner <areg06.06@mail.ru>
2025-10-08 16:06:25 +0300

    fix dockerfile and analitic websocket

--- frontend/src/components/Analitic.tsx
+++ frontend/src/components/Analitic.tsx
@@ -1,43 +1,53 @@
 "use client";
 
+import { hostname } from "os";
 import { useEffect } from "react";
 
 type Props = {
-  tag?: string;
+	tag?: string;
 };
 
 export function Analitic({ tag }: Props) {
-  useEffect(() => {
-    console.log("++++++++++++++++++++++++++++++++++++++++++++++++++");
-    console.log(tag);
-    var url = tag ? "/api/v1/session/" + tag : "/api/v1/session";
+	useEffect(() => {
+		var url = tag ? "/api/v1/session/" + tag : "/api/v1/session";
 
-    fetch(url, { method: "POST" }).then((r) => {
-      if (r.status === 200) {
-        const socket = new WebSocket("ws://localhost:7080/api/v1/analitics/ws");
-      }
-    });
-    const handleVisibilityChange = () => {
-      if (document.visibilityState === "hidden") {
-        console.log("Пользователь ушёл со страницы");
-      } else {
-        console.log("Пользователь вернулся");
-      }
-    };
+		fetch(url, { method: "POST" }).then((r) => {
+			if (r.status === 200) {
+				let socket: WebSocket
+				let protocol: string
+				let port: string = ""
 
-    const handleWindowFocus = () => console.log("Окно в фокусе");
-    const handleWindowBlur = () => console.log("Окно потеряло фокус");
+				if (location.protocol === 'https:') {
+					protocol = "wss://"
+				} else {
+					protocol = "ws://"
+				}
 
-    document.addEventListener("visibilitychange", handleVisibilityChange);
-    window.addEventListener("focus", handleWindowFocus);
-    window.addEventListener("blur", handleWindowBlur);
+				socket = new WebSocket(protocol + location.host + "/api/v1/analitics/ws");
+			}
+		});
 
-    return () => {
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-      window.removeEventListener("focus", handleWindowFocus);
-      window.removeEventListener("blur", handleWindowBlur);
-    };
-  }, []);
+		// const handleVisibilityChange = () => {
+		// 	if (document.visibilityState === "hidden") {
+		// 		console.log("Пользователь ушёл со страницы");
+		// 	} else {
+		// 		console.log("Пользователь вернулся");
+		// 	}
+		// };
 
-  return <></>;
+		// const handleWindowFocus = () => console.log("Окно в фокусе");
+		// const handleWindowBlur = () => console.log("Окно потеряло фокус");
+
+		// document.addEventListener("visibilitychange", handleVisibilityChange);
+		// window.addEventListener("focus", handleWindowFocus);
+		// window.addEventListener("blur", handleWindowBlur);
+
+		return () => {
+			// document.removeEventListener("visibilitychange", handleVisibilityChange);
+			// window.removeEventListener("focus", handleWindowFocus);
+			// window.removeEventListener("blur", handleWindowBlur);
+		};
+	}, []);
+
+	return <></>;
 }
